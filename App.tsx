b123ba09005aa7719dd86271e388b580040517d5
jocelyn pyw <jocelynwotcheu2@gmail.com>
2022-12-14 17:29:16 +0100

    page splash deja okay

--- App.tsx
+++ App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Onboarding from './src/screens/Onboarding';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Splash from './src/screens/splash';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Onboarding} />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Splash} />
+        <Stack.Screen name="Onboarding" component={Onboarding} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,14 +1,30 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Image, Dimensions} from 'react-native';
 import React from 'react';
+import {colors} from '../constants/colors';
 
 const Splash = () => {
   return (
-    <View>
-      <Text>splash</Text>
+    <View style={styles.container}>
+      <View style={styles.topcontainer} />
+      <Image source={require('../assets/Logo.png')} style={styles.image} />
     </View>
   );
 };
 
 export default Splash;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  topcontainer: {
+    height: Dimensions.get('screen').height / 5,
+  },
+  image: {
+    height: 150,
+    width: 350,
+  },
+});
